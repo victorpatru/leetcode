953dd85db18b74e720950ef50ef099cad899048e
Victor Patru <patruvictor.mihai@gmail.com>
2025-11-28 13:52:35 +0200

    fix: add jsdocs on the dynamic array primitive

--- core-algorithms/dynamic-array/dynamic-array.template.js
+++ core-algorithms/dynamic-array/dynamic-array.template.js
@@ -1,7 +1,25 @@
 /**
- * Dynamic Array Class
- * Note: JavaScript arrays are dynamic by default,
- * but this class is used to simulate the behavior of a dynamic array.
+ * Dynamic Array (aka a resizable array) class, such as an ArrayList in Java or a vector in C++.
+ * 
+ * The DynamicArray class supports the following operations:
+ * 
+ * - DynamicArray(int capacity) will initialize an empty array with a capacity of capacity, where capacity > 0.
+ * 
+ * - int get(int i) will return the element at index i. Assume that index i is valid.
+ * 
+ * - void set(int i, int n) will set the element at index i to n. Assume that index i is valid.
+ * 
+ * - void pushback(int n) will push the element n to the end of the array.
+ * 
+ * - int popback() will pop and return the element at the end of the array. Assume that the array is non-empty.
+ * 
+ * - void resize() will double the capacity of the array.
+ * 
+ * - int getSize() will return the number of elements in the array.
+ * 
+ * - int getCapacity() will return the capacity of the array.
+ * 
+ * Note: If we call void pushback(int n) but the array is full, we should resize the array first.
  */
 class DynamicArray {
     /**
